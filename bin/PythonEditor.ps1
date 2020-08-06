8e88aa923e6e7f402768d6c0a14fb7056476fe12
cundesi <cundesi.td@gmail.com>
2020-08-06 11:34:50 +0800

    update: use rez

--- bin/PythonEditor.ps1
+++ bin/PythonEditor.ps1
@@ -1,4 +1,1 @@
-﻿if (!(Test-Path Variable:PSScriptRoot)) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
-$python_app = "python"
-$app_args = '-m PythonEditorLaunch'
-if($myinvocation.expectingInput) { $input | & $python_app $app_args @args } else { & $python_app $app_args @args }
+python -m PythonEditorLaunch
--- python/PythonEditorLaunch.py
+++ python/PythonEditorLaunch.py
@@ -8,7 +8,6 @@ import sys
 import os
 import time
 
-
 sys.dont_write_bytecode = True
 start = time.time()
 
@@ -18,35 +17,46 @@ os.environ['PYTHONEDITOR_CAPTURE_STARTUP_STREAMS'] = '1'
 # we can now import the package in earnest.
 from PythonEditor.ui import ide
 from PythonEditor.ui.features import ui_palette
-from PythonEditor.ui.Qt import QtWidgets, QtGui
+from PythonEditor.ui.Qt import QtWidgets
+from PythonEditor.ui.Qt import QtGui
+from PythonEditor.ui.Qt import QtCore
 
-try:
-    app = QtWidgets.QApplication(sys.argv)
-except RuntimeError:
-    # for running inside an existing app
+
+def main(set_styles=True):
     app = QtWidgets.QApplication.instance()
+    if not app:
+        app = QtWidgets.QApplication(sys.argv)
 
-PDF = 'PYTHONEDITOR_DEFAULT_FONT'
-os.environ[PDF] = 'Consolas'
-_ide = ide.IDE()
-app.setPalette(ui_palette.get_palette_style())
-_ide.showMaximized()
+    PDF = 'PYTHONEDITOR_DEFAULT_FONT'
+    os.environ[PDF] = 'Consolas'
+    _ide = ide.IDE()
+    _ide.setParent(app.activeWindow())
+    _ide.setWindowFlags(QtCore.Qt.Window)
+    if set_styles:
+        app.setPalette(ui_palette.get_palette_style())
+    _ide.showMaximized()
 
-# Plastique isn't available on Windows, so try multiple styles.
-styles = QtWidgets.QStyleFactory.keys()
-style_found = False
-for style_name in ['Plastique', 'Fusion']:
-    if style_name in styles:
-        print('Setting style to:', style_name)
-        style_found = True
-        break
+    # Plastique isn't available on Windows, so try multiple styles.
+    styles = QtWidgets.QStyleFactory.keys()
+    style_found = False
+    for style_name in ['Plastique', 'Fusion']:
+        if style_name in styles:
+            print('Setting style to:', style_name)
+            style_found = True
+            break
 
-if style_found:
-    style = QtWidgets.QStyleFactory.create(style_name)
-    QtWidgets.QApplication.setStyle(style)
+    if style_found:
+        style = QtWidgets.QStyleFactory.create(style_name)
+        QtWidgets.QApplication.setStyle(style)
 
-print(
-    'PythonEditor import time: %.04f seconds'
-    % (time.time()-start)
-)
-sys.exit(app.exec_())
+    print('PythonEditor import time: %.04f seconds' % (time.time() - start))
+    if app.applicationName() in ['python', 'mayapy']:
+        sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        import traceback
+        traceback.print_exc()
